52487d73a351bf06faab970b75e4c80799f7c71f
Sanjeev Saini <sanjeevsaini11235@gmail.com>
2019-08-07 14:32:55 +0530

    st p2

---
+++ differentScope/src/main/java/com/san/beans/DishonestConductor.java
@@ -0,0 +1,18 @@
+package com.san.beans;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.stereotype.Component;
+
+@Component
+@Lazy
+public class DishonestConductor extends Conductor {
+	
+	@Autowired
+	private Ticket ticket;
+	
+	@Override	
+	public Ticket getTicket() {
+		return ticket;
+	}	
+}
